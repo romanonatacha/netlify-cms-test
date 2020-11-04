c60a2503507a63e25c254ca7195443f344df6762
Natacha Romano <natacharomanonr@gmail.com>
2020-11-04 01:03:15 -0300

    removing ui

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import data from '../data/data.json'
+// import data from '../data/data.json'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <ul>
+      {/* <ul>
         {data.first.map(x => (
           <li>{x.title} | {x.description}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
